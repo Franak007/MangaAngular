768d180ae1ccc56b295c9b11d470f45f2181caa2
Franak007 <franck.pertosa@gmail.com>
2023-09-05 12:50:26 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MangaListComponent} from "./manga-list/manga-list.component";
+import {MangaDetailsComponent} from "./manga-details/manga-details.component";
+import {Error404Component} from "./error404/error404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'liste', component: MangaListComponent},
+  {path: 'liste/:id', component: MangaDetailsComponent},
+  {path: '**', component: Error404Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MangaListComponent } from './manga-list/manga-list.component';
+import { MangaDetailsComponent } from './manga-details/manga-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MangaListComponent,
+    MangaDetailsComponent,
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
